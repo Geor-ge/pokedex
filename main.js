35c726c018c91683de9a207ad8f0b130f48284bb
George Bayley <mzpmgdkqsrnb@opayq.com>
2018-11-08 00:38:06 -0500

    ajax calls &  trainer methods working in console

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-
 class Trainer {
-  constructor() {
-    this.name = "Bruce Leeroy",
-    this.gym = "The Dojo",
+  constructor(gym, trainer, pokemon) {
+    this.gym = gym,
+    this.trainer = trainer,
     this.pokemon = [];
 
     console.log("new trainer added");
@@ -11,9 +10,9 @@ class Trainer {
     return this.pokemon;
   }
   get(name) {
-    for (i=0; i<pokemon;) {
-      if(name === pokemon[i]["name"]) {
-        return pokemon[i];
+    for (let i=0; i<this.pokemon.length;) {
+      if(name === this.pokemon[i]["name"]) {
+        return this.pokemon[i];
       }else {
         i ++;
       }
@@ -27,7 +26,9 @@ class Pokemon {
     this.hp = hp,
     this.attack = attack,
     this.defense = defense,
-    this.abilities = []
+    this.abilities = abilities,
+    this.gym = gymLeader.gym,
+    this.trainer = gymLeader.trainer
   }
 }
 
@@ -47,8 +48,8 @@ function getSnorlax() {
           data["abilities"]["2"]["ability"]["name"]
         ]
       );
-        gymLeader.pokemon.push(snorlax);
-        return console.log("snorlax added to pokedex")
+      gymLeader.pokemon.push(snorlax);
+      return console.log("Snorlax added to pokedex")
 
     }
   };
@@ -56,13 +57,54 @@ function getSnorlax() {
   xhttp.send();
 }
 
-document.addEventListener("onload",  gymLeader = new Trainer(), getSnorlax())
+function getSawk() {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+      let sawk = new Pokemon (
+        data["name"],
+        data["stats"]["5"]["base_stat"],
+        data["stats"]["4"]["base_stat"],
+        data["stats"]["3"]["base_stat"],
+        [
+          data["abilities"]["0"]["ability"]["name"],
+          data["abilities"]["1"]["ability"]["name"],
+          data["abilities"]["2"]["ability"]["name"]
+        ]
+      );
+        gymLeader.pokemon.push(sawk);
+        return console.log("Sawk added to pokedex")
 
+    }
+  };
+  xhttp.open("GET","https://fizal.me/pokeapi/api/v2/name/sawk.json", true);
+  xhttp.send();
+}
 
-// addPokemon (name,hp,atk,def,abilities) {
-  //   //bracket notation pulls name from function input and sets it as key
-  //   this.pokedex.push({[name]:{
-  //     "hp":hp,"atk":atk,"def":def,"abilities":[abilities]}});
-  //     console.log(`${name} added to pokedex`);
-  //   }
-  // }
+function getHit() {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+      let hitmonlee = new Pokemon (
+        data["name"],
+        data["stats"]["5"]["base_stat"],
+        data["stats"]["4"]["base_stat"],
+        data["stats"]["3"]["base_stat"],
+        [
+          data["abilities"]["0"]["ability"]["name"],
+          data["abilities"]["1"]["ability"]["name"],
+          data["abilities"]["2"]["ability"]["name"]
+        ]
+      );
+        gymLeader.pokemon.push(hitmonlee);
+        return console.log("Hitmonlee added to pokedex")
+
+    }
+  };
+  xhttp.open("GET","https://fizal.me/pokeapi/api/v2/name/hitmonlee.json", true);
+  xhttp.send();
+}
+
+document.addEventListener("onload",  gymLeader = new Trainer("The Dojo", "Bruce Leeroy"), getSnorlax(), getSawk(), getHit());
